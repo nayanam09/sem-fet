eb1e6352ca24ec50b4b2be72b1768ba5eaf6d1d8
nayanam09 <nayanam901@gmail.com>
2026-05-23 11:39:27 +0530

    Add ticket booking functionality with popup

---
+++ assignments/project/script.js
@@ -0,0 +1,25 @@
+const popup = document.getElementById("popup");
+const matchName = document.getElementById("matchName");
+const bookingForm = document.getElementById("bookingForm");
+const message = document.getElementById("message");
+
+function bookTicket(match) {
+  popup.style.display = "flex";
+  matchName.value = match;
+}
+
+function closePopup() {
+  popup.style.display = "none";
+  message.innerHTML = "";
+}
+
+bookingForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const tickets = document.getElementById("tickets").value;
+
+  message.innerHTML = `Tickets  are booked successfully`;
+
+  bookingForm.reset();
+});
